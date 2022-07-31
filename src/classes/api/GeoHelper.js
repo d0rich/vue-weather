bd7c6eaf28f39f36de2a17960fc34a7263dd7297
Nikolay Dorofeev <dorich2000@gmail.com>
2022-07-31 22:16:32 +0400

    URL to HTTPS proxy of GeoHelper

--- src/classes/api/GeoHelper.js
+++ src/classes/api/GeoHelper.js
@@ -3,9 +3,10 @@ import {City, Region} from "@/classes";
 
 export class GeoHelper{
     apiKey = 'QcCPBQrO82z8m2QIuMfaUMLz7OamwVs8'
+    apiHost = 'https://geohelper-proxy.onrender.com'
 
     async getRegionById(id){
-        let req = `http://geohelper.info/api/v1/regions?apiKey=${this.apiKey}`
+        let req = `${this.apiHost}/api/v1/regions?apiKey=${this.apiKey}`
         req += '&locale[lang]=ru'
         req += `&filter[id]=${id}`
         let res = await axios.get(req)
@@ -14,7 +15,7 @@ export class GeoHelper{
     }
 
     async getRegions(){
-        let req = `http://geohelper.info/api/v1/regions?apiKey=${this.apiKey}`
+        let req = `${this.apiHost}/api/v1/regions?apiKey=${this.apiKey}`
         req += '&locale[lang]=ru'
         req += '&filter[countryIso]=ru'
         req += `&pagination[page]=1`
@@ -30,7 +31,7 @@ export class GeoHelper{
     }
 
     async getCityById(id){
-        let req = `http://geohelper.info/api/v1/cities?apiKey=${this.apiKey}`
+        let req = `${this.apiHost}/api/v1/cities?apiKey=${this.apiKey}`
         req += '&locale[lang]=ru'
         req += `&filter[id]=${id}`
         let res = await axios.get(req)
@@ -42,7 +43,7 @@ export class GeoHelper{
     }
 
     async getCityByName(name){
-        let req = `http://geohelper.info/api/v1/cities?apiKey=${this.apiKey}`
+        let req = `${this.apiHost}/api/v1/cities?apiKey=${this.apiKey}`
         req += '&locale[lang]=ru'
         req += `&filter[name]=${name}`
         req += '&filter[nameStrictLanguage]=ru'
@@ -50,6 +51,16 @@ export class GeoHelper{
         req += '&order[dir]=desc'
         let res = await axios.get(req)
         let city = res.data.result[0]
+        if (!city) {
+            let req = `${this.apiHost}/api/v1/cities?apiKey=${this.apiKey}`
+            req += '&locale[lang]=ru'
+            req += `&filter[name]=Москва`
+            req += '&filter[nameStrictLanguage]=ru'
+            req += '&order[by]=population'
+            req += '&order[dir]=desc'
+            let res = await axios.get(req)
+            city = res.data.result[0]
+        }
         let cityAsClass = new City(city.id, city.name, null, city?.postCode, city?.population)
         cityAsClass.region = new Region()
         cityAsClass.region.id = city.regionId
@@ -57,7 +68,7 @@ export class GeoHelper{
     }
 
     async getCitiesOfRegion(regionId, page = 1, limit = 20, orderBy = 'name', dir = 'asc'){
-        let req = `http://geohelper.info/api/v1/cities?apiKey=${this.apiKey}`
+        let req = `${this.apiHost}/api/v1/cities?apiKey=${this.apiKey}`
         req += '&locale[lang]=ru'
         req += `&filter[regionId]=${regionId}`
         req += `&pagination[page]=${page}`
@@ -74,17 +85,24 @@ export class GeoHelper{
     }
 
     async getTopCities(country_code){
-        let req = `http://geohelper.info/api/v1/cities?apiKey=${this.apiKey}`
+        let req = `${this.apiHost}/api/v1/cities?apiKey=${this.apiKey}`
         req += '&locale[lang]=ru'
         req += `&filter[countryIso]=${country_code}`
         req += `&order[by]=population`
         req += `&order[dir]=desc`
         let res = await axios.get(req)
-        let cities = []
-        res.data.result.forEach(city => {
-            let newCity = new City(city.id, city.name, null, city?.postCode, city?.population)
-            cities.push(newCity)
-        })
+        let cities = res.data.result
+            .map(city => new City(city.id, city.name, null, city?.postCode, city?.population))
+        if (!cities.length) {
+            let req = `${this.apiHost}/api/v1/cities?apiKey=${this.apiKey}`
+            req += '&locale[lang]=ru'
+            req += `&filter[countryIso]=RU`
+            req += `&order[by]=population`
+            req += `&order[dir]=desc`
+            let res = await axios.get(req)
+            cities = res.data.result
+                .map(city => new City(city.id, city.name, null, city?.postCode, city?.population))
+        }
         return cities
     }
 }
